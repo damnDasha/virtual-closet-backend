4f637911583428a93591a88ccc9629f78af878f0
dasha akhten <dasha.akhten@gmail.com>
2020-10-31 20:02:31 -0700

    feat: migration, seeds, users, db

---
+++ seeds/seed.outfits.sql
@@ -0,0 +1,7 @@
+INSERT INTO outfits (id, content, events_id)
+VALUES
+    (14, 'black dress', 2),
+    (3, 'jeans', 7),
+    (4, 'pascal top isabel marant pants', 5),
+    (1, 'balenciaga dress, goyard', 1),
+    (134, 'IRO suede pants with giambattista top', 4);
